0a568f1a9634b3fb3ce4c7625f9e904c2d983540
Ubuntu <ubuntu@ip-172-31-31-234.ap-southeast-2.compute.internal>
2024-12-10 12:00:05 +0000

    node project

---
+++ nodejs-project/app.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('./index');
+
+test('GET / should return Hello, World!', async () => {
+  const response = await request(app).get('/');
+  expect(response.text).toBe('Hello, World!');
+});
+
